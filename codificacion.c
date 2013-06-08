7ad2d25d21c37f42f5622ff20bc094c209741842
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-08 23:20:07 +0000

    Por ahí lei que trabajan con unsigned char, asi que quizas haya que cambiarlo

--- codificacion.c
+++ codificacion.c
@@ -1,23 +1,18 @@
 #include <math.h>
+//#include <stdio.h>
 
-
-char* unary_code(int number) {
+void unary_code(int number, char* unary) {
 	int x = number - 1;
-	char unary[number+1];
 	int p = 0;
-	
 	while(p < x) {
 		unary[p] = '1';
 		p++;
 	}
 	unary[x] = '0';
 	unary[number] = '\0';
-	
-	return unary;
 }
 
-char* binary_code(int number, int bits) {
-	char* binary[bits+1];
+void binary_code(int number, int bits, char* binary) {
 	int division = number;
 	
 	for(int x = 0; x < bits; x++) {
@@ -29,19 +24,16 @@ char* binary_code(int number, int bits) {
 		}
 	}
 	binary[bits] = '\0';
-	
-	return binary;
 }
 
-char* base_code(int number, char* func(int)) {
+void base_code(int number, void func(int,char*), char* vec) {
 	int bits = (int)floor(log10(number)/log10(2)); // log(number) en base 2
 	int q = 1+bits;
-	char* vec[q+bits+1];
-	strcpy(vec,func(q));
+	func(q,vec);
 	
 	int bin = number - (int)pow(2,(double)bits);
-	char* binary[bits+1];
-	strcpy(binary, binary_code(bin,bits));
+	char binary[bits+1];
+	binary_code(bin,bits,binary);
 	int x = 0;
 	
 	for(int i = q; i < (q+bits+1); i++) {
@@ -49,14 +41,24 @@ char* base_code(int number, char* func(int)) {
 		x++;
 	}
 	vec[q+bits] = '\0';
+}
+
+void gamma_code(int number, char* gamma) {
+	base_code(number,unary_code,gamma);
+}
+
+void delta_code(int number, char* delta) {
+	base_code(number,gamma_code,delta);	
+}
+/*
+int main() {
+
+	char buff[100] = " ";
 	
-	return vec;
-}
+	unary_code(10,buff);
+	
+	printf("%s",buff);
+	
+	return 0;
+}*/
 
-char* gamma_code(int number) {
-	return base_code(number,unary_code);
-}
-
-char* delta_code(int number) {
-	return base_code(number,gamma_code);	
-}
