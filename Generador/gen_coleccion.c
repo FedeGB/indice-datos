e50e26ca3487d69b43fffb5b5884436980874273
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-11 16:53:47 +0000

    -Añadida una estructura (documentos.c/documentos.h) para cuando se procesan los documentos (para tener la informacion importante en una misma estructura). -Agregada funcion fsize que indica el peso de un archivo (gen_coleccion.c)

--- Generador/gen_coleccion.c
+++ Generador/gen_coleccion.c
@@ -1,6 +1,7 @@
 #include <dirent.h>
 #include <string.h>
-#include "codigos_retorno.h"
+#include "../indice-datos/codigos_retorno.h"
+#include "documentos.h"
 
 #define BUFF_SIZE 500
 
@@ -24,21 +25,46 @@ int leerColeccionPath(char* ruta){
 	char aux[BUFF_SIZE];
 	char antes[BUFF_SIZE];
 	FILE* arch;
-
+	doc_t* doc_ac = NULL;
+	int size = 0;
+	
 	strncpy(antes, path, strlen(path));	//Carpeta base
 
 	while(1) {
 		file = readdir(directorio);
-		if(!file) break;
+		if(!file) return RES_ERROR;
 		if(strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0)
 			continue;
 		snprintf(aux, strlen(antes)+strlen(file->d_name)+2, "%s/%s", antes, file->d_name);
 		strcpy(path,aux);
 		if(leerColeccionPath(path) != RES_OK){ //Si no es un directorio continuamos a procesar el archivo
-			// Procesar archivo de la coleccion
+			arch = fopen(path,"r");
+			if(!arch)
+				return RES_NULL;
+			
+			size = fsize(arch);
+			doc_ac = crear_documento(path,file->d_name,size);
+			// Metemos documento en una lista de forma que se ordene por "size" de mayor a menor
+			// Y metemos en otra que lo ordene en orden alfabetico
+			
+			
 		}		
 
 	}
 	closedir(directorio);
 	return RES_OK;
+}
+
+// Retorna el peso del archivo
+int fsize(FILE* fp) {
+	
+	if(!fp)
+		return RES_NULL;
+		
+	int size = 0;
+	fseek(fp,0,SEEK_END);
+	size = ftell(fp);
+	fseek(fp,0,SEEK_SET);
+	
+	return size;
 }
