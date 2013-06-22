49f793d4d2c500bd45f0a1afe15796dbd45d9152
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-22 00:59:28 +0000

    Ahora el decodificador recibe un puntero el cual contendra q tanto caracteres del array utilizo para decodificar. Tengo problemas con el manejo de escritura y lectura en archivos (con bits y demas

--- codificacion.c
+++ codificacion.c
@@ -6,7 +6,7 @@ typedef unsigned char uchar;
 
 // Escribe un numero en unario
 // recibe un buffer en donde depositara el codigo unario
-void unary_code(long int number, uchar* unary) {
+int unary_code(long int number, uchar* unary) {
 	int x = number - 1;
 	int p = 0;
 	while(p < x) {
@@ -15,6 +15,7 @@ void unary_code(long int number, uchar* unary) {
 	}
 	unary[x] = '0';
 	unary[number] = '\0';
+	return (int)number;
 }
 
 // Decodifica un codigo unario
@@ -29,7 +30,7 @@ long int unary_decode(uchar* unary) {
 // Recibe un buffer en donde depositara el codigo binario
 // Pre: el numero debe poder representarse con la cantidad de bits indicada
 // Post: El codigo binario devuelto se lee de izquierda a derecha
-void binary_code(long int number, int bits, uchar* binary) {
+int binary_code(long int number, int bits, uchar* binary) {
 	int division = number;
 	
 	for(int x = 0; x < bits; x++) {
@@ -42,6 +43,7 @@ void binary_code(long int number, int bits, uchar* binary) {
 		division = division/2;
 	}
 	binary[bits] = '\0';
+	return bits;
 }
 
 // Decodifica un codigo binario
@@ -59,7 +61,7 @@ long int binary_decode(uchar* binary) {
 
 // Funcion que genera un codigo con una primera parte en codigo especificado (func)
 // y la segunda parte en codigo binario
-void base_code(long int number, void func(int,uchar*), uchar* vec) {
+int base_code(long int number, int func(long int,uchar*), uchar* vec) {
 	int bits = (int)floor(log10(number)/log10(2)); // log(number) en base 2
 	int q = 1+bits;
 	func(q,vec);
@@ -78,16 +80,18 @@ void base_code(long int number, void func(int,uchar*), uchar* vec) {
 		x++;
 	}
 	vec[pre+bits] = '\0';
+	return (pre+bits);
 }
 
 // Escribe un numero en codigo gamma
 // Recibe un buffer en el cual se depositara el codigo gamma
-void gamma_code(long int number, uchar* gamma) {
-	base_code(number,unary_code,gamma);
+int gamma_code(long int number, uchar* gamma) {
+	return base_code(number,unary_code,gamma);
 }
 
-// Decodifica un codigo gamma
-long int gamma_decode(uchar* gamma) {
+// Decodifica un codigo gamma, y se le pasa un puntero a un entero para saber
+// cuantos uchar decodifico del array
+long int gamma_decode(uchar* gamma, int* done) {
 	int len_max = strlen((char*)gamma);
 	uchar pre[len_max]; // No conozco el maximo a priori pero no es mayor al gamma recibido
 	memset(pre,' ',len_max);
@@ -110,29 +114,32 @@ long int gamma_decode(uchar* gamma) {
 		i++;
 	}
 	post[i++] = '\0';
+	*done = y;
 	return (int)pow(2,(double)q) + binary_decode(post);
 }
 
 // Escribe un numero en codigo delta
 // Recibe un buffer en el cual se dpositara el codigo delta
-void delta_code(long int number, uchar* delta) {
-	base_code(number,gamma_code,delta);	
+int delta_code(long int number, uchar* delta) {
+	return base_code(number,gamma_code,delta);	
 }
 
 // Decodifica un codigo delta
 long int delta_decode(uchar* delta) {
-	int len_max = strlen((char*)delta);
-	int q = gamma_decode(delta);
+	//int len_max = strlen((char*)delta);
+	int* w = malloc(sizeof(int));
+	int q = gamma_decode(delta, w);
 	q = q-1;
 	uchar post[q+1];
 	memset(post,' ',q+1);
-	int x = len_max-q;
+	
 	int y = 0;
-	for(int t = x; t < len_max; t++) {
+	for(int t = *w; t < (*w)+q-1; t++) {
 		post[y] = delta[t];
 		y++;
 	}
 	post[y++] = '\0';
+	free(w);
 	return (int)pow(2,(double)q) + binary_decode(post);
 }
 
