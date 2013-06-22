51543a5ba1c5425adba93d7685952505721ff67c
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-22 23:43:17 +0000

    Creo que el manejo_bin ya funciona, aunque hay un problema con el array en uchar cuando cargas para leer bytes

--- manejo_bin.c
+++ manejo_bin.c
@@ -6,23 +6,21 @@
 int escribir_binf(FILE* fd, uchar* array, size_t size) {
 	if(!fd)
 		return RES_NULL;
-	/*if(size%8 == 0) {
-		fwrite(array, 1, size, fd); 
-		return RES_OK;
-	}*/
 	uchar buffer = 0;
 	int bit_actual = 0;
 	int x = 0;
 	while(x < (int)size) {
 		if((x+1)%8 == 0) {
-			if(array[x]) {
+			if(array[x] & 1)
 				buffer |= (1<<bit_actual);
 			fwrite(&buffer, 1, 1, fd);
+			buffer &= 0;
 			bit_actual = 0;
 			x++;
+			continue;
 		}
-		if(array[x]) {
-				buffer |= (1<<bit_actual);
+		if(array[x] & 1)
+			buffer |= (1<<bit_actual);
 		x++;
 		bit_actual++;
 	}
@@ -31,7 +29,9 @@ int escribir_binf(FILE* fd, uchar* array, size_t size) {
 		x++;
 		bit_actual++;
 	}
-	fwrite(&buffer, 1, 1, fd);
+	if(x%8 == 0) {
+		fwrite(&buffer, 1, 1, fd);
+	}
 	
 	return RES_OK;
 }
@@ -39,22 +39,22 @@ int escribir_binf(FILE* fd, uchar* array, size_t size) {
 int leer_binf(FILE* fd, uchar* buffer, size_t size, long int offset) {
 	if(!fd)
 		return RES_NULL;
-	uchar char_buff[size];
-	memset(char_buff, '\0', size);
+	uchar char_buff = 0;
 	fseek(fd, offset, SEEK_SET);
-	fread(buffer, 1, size, fd);
 	int bit_actual = 0;
 	for(int x = 0; x<(int)size; x++) {
+		fread(&char_buff, 1, 1, fd);
 		while(bit_actual < 8) {	
-			if(char_buff[x] & (1<<bit_actual) != 0) {
-				buffer[bit_actual+(8*x)] = 1;
+			if((char_buff & (1<<bit_actual))) {
+				buffer[bit_actual+(8*x)] |= 1;
 			}
 			else {
-				buffer[bit_actual+(8*x)] = 0;
+				buffer[bit_actual+(8*x)] |= 0;
 			}
 			bit_actual++;
 		}
 		bit_actual = 0;
+		char_buff = 0;
 	}
 	
 	return RES_OK;
