e9b79d3ebc55cf769ad79e86c0b85a3129d8eef7
ivanjqk@gmail.com <ivanjqk@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 00:44:05 +0000

    Iván probando el SVN del Eclipse.

--- TP/src/TP.cpp
+++ TP/src/TP.cpp
@@ -1,17 +1,16 @@
 //============================================================================
 // Name        : TP.cpp
-// Author      : 
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Author      : GRUPO 19
+// Version     : 1.0
+// Copyright   : No copyright
+// Description : Generador de índices del TP de Organización de Datos (75.06)
 //============================================================================
 
 // https://docs.google.com/file/d/0B8rBD4QSqWnSdjlEa2V3YXV0WHc/edit?usp=sharing
 
 #include <iostream>
-using namespace std;
 
 int main() {
-	cout << "" << endl; // prints 
-	return 0;
+	std::cout << "HOLA" << std::endl;
+	return 2;
 }
