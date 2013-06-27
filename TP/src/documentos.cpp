a2d6671c4589bab8db6fa13ca788e48c5455b449
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 03:04:10 +0000

    Documentos

--- TP/src/documentos.cpp
+++ TP/src/documentos.cpp
@@ -6,17 +6,11 @@
  */
 
 #include "./documentos.h"
-#include <cstring>
 
-Documentos::Documentos(char* path, char* name, int size) {
-  this->path = new char[strlen(path) + 1];
-  strcpy(this->path, path);
-  this->name = new char[strlen(name) + 1];
-  strcpy(this->name, name);
-  this->size = size;
+Documento::Documento(const std::string &path, const std::string &name, int size): 
+					path(path), name(name), size(size) { }
+
+void Documento::setSerial(int num) {
+	this->serial = num;
 }
 
-Documentos::~Documentos() {
-  delete[] path;
-  delete[] name;
-}
