d09293305ae2fc3fc03964b032c7d406cd8e8277
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 22:35:32 +0000

    ya casi...

--- trunk/TP/src/Termino.h
+++ trunk/TP/src/Termino.h
@@ -2,22 +2,19 @@
 #define TERMINO_H
 #include <string>
 #include <set>
+#include <iostream>
 
-bool ordenarNumericamente(const int primer, const int segundo);
+class Localizacion;
+class Termino;
 
-class Localizacion {
+class Ordenamiento {
+	private:
+		Ordenamiento();
 	public:
-		std::set< int, bool (*)(const int primer, const int segundo) > posiciones(ordenarNumericamente);
-		int numeroDoc;
-		int frecuenciaLocal;
-		
-		Localizacion(int numeroDoc);
-		void agregarPosicion(int pos);
-		void aumentarFrecuencia();
-		bool ordenarPorNumeroDoc(const Localizacion &primer, const Localizacion &segundo);
-		void unirLocalizacion(Localizacion &local);
-		
-		virtual ~Localizacion();
+	virtual ~Ordenamiento();
+	static bool ordenarTerminoAlfabeticamente(const Termino &primer, const Termino &segundo);
+	static bool ordenarPorNumeroDoc(const Localizacion &primer, const Localizacion &segundo);
+	static bool ordenarNumericamente(const int primer, const int segundo);
 	
 };
 
@@ -25,17 +22,32 @@ class Termino {
 	public:
 		std::string termino;
 		unsigned long int frecuenciaGlobal;
-		std::set< Localizacion, bool (*)(const Localizacion &primer, const Localizacion &segundo) > localizaciones(ordenarPorNumeroDoc);
+		std::set< Localizacion, bool (*)(const Localizacion &primer, const Localizacion &segundo) > localizaciones(Ordenamiento::ordenarPorNumeroDoc);
 		
 		Termino(const std::string &str);
 		
 		void aumentarFrecuencia();
 		
-		bool ordenarTerminoAlfabeticamente(const Termino &primer, const Termino &segundo);
 		
 		void agregarLocalizacion(Localizacion &local);
 		
 		virtual ~Termino();
+};
+
+class Localizacion {
+	public:
+		std::set< int, bool (*)(const int primer, const int segundo) > posiciones(Ordenamiento::ordenarNumericamente);
+		int numeroDoc;
+		int frecuenciaLocal;
+		
+		Localizacion(int numeroDoc);
+		void agregarPosicion(int pos);
+		void aumentarFrecuencia();
+		void unirLocalizacion(Localizacion &local);
+		
+		
+		virtual ~Localizacion();
+	
 };
 
 #endif
