25658fbc3e594eb4981a2c4f1e3f23297c56b1cf
ivanjqk@gmail.com <ivanjqk@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 05:37:10 +0000

    Emprolijo un poco más.

--- TP/src/Consultar.cpp
+++ TP/src/Consultar.cpp
@@ -9,6 +9,7 @@
 // La colección de documentos de muestra está en:
 // https://docs.google.com/file/d/0B8rBD4QSqWnSdjlEa2V3YXV0WHc/edit?usp=sharing
 
+#include <cstring>
 #include <iostream>
 #include <string>
 #include "./Indexador.h"
@@ -21,18 +22,43 @@ int OTROmain(int argc, char *argv[]) {
     retornoValor = ERROR_ARGC;
     std::cout << "FÍN DE PROGRAMA CONTROLADO, ECHO $? = " << retornoValor << std::endl;
     return retornoValor;
-  } else {
-    // TODO (): Parsear argv, ver que diga -r y -q, y obtener el string del nombre del repositorio y el string de la consulta sin comillas.
-    std::cout << "CANTIDAD DE ARGUMENTOS CORRECTA." << std::endl;
-    std::string repositorioNombre(argv[2]);
-    std::cout << "El nombre del repositorio es: " << repositorioNombre << std::endl;
-    std::string consulta("ACÁ VA LA CONSULTA");
-    std::cout << "La consulta es por: \"" << consulta << "\"" << std::endl;
   }
+  if (strcmp(argv[1], "-r") != 0) {
+    std::cout << "ARGUMENTO INCORRECTO: " << argv[1] << std::endl;
+    retornoValor = ERROR_ARGV;
+    std::cout << "FÍN DE PROGRAMA CONTROLADO, ECHO $? = " << retornoValor << std::endl;
+    return retornoValor;
+  }
+  if (strcmp(argv[3], "-q") != 0) {
+    std::cout << "ARGUMENTO INCORRECTO: " << argv[3] << std::endl;
+    retornoValor = ERROR_ARGV;
+    std::cout << "FÍN DE PROGRAMA CONTROLADO, ECHO $? = " << retornoValor << std::endl;
+    return retornoValor;
+  }
+  std::cout << "CANTIDAD DE ARGUMENTOS CORRECTA." << std::endl;
+  std::string indiceNombre(argv[2]);
+  std::cout << "El nombre del repositorio es: " << indiceNombre << std::endl;
+
+  // TODO (): Parsear los argumentos para obtener la consulta.
+  std::string consulta("*****************ACÁ VA LA CONSULTA*****************");
+
+  std::cout << "La consulta es por: \"" << consulta << "\"" << std::endl;
   std::string indiceRuta("./");
-  //Consultador consultador(indiceNombre, indiceRuta;
+
+  //Consultador consultador(indiceNombre, indiceRuta);
+
   {
+
+    if (/*consultador.abrirIndice() !=*/RESULTADO_OK) {
+
+      std::cout << "INDICE: " << indiceNombre << " NO EXISTE EN UBICACIÓN: " << indiceRuta << std::endl;
+      retornoValor = ERROR;
+      std::cout << "FÍN DE PROGRAMA CONTROLADO, ECHO $? = " << retornoValor << std::endl;
+      return retornoValor;
+    }
+
     //consultador.consultar(consulta);
+
   }
   retornoValor = RESULTADO_OK;
   std::cout << "FÍN DE PROGRAMA CONTROLADO, ECHO $? = " << retornoValor << std::endl;
