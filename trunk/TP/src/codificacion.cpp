9330b16417ed5f47f6bd71beebb1773c98dc472f
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 08:53:45 +0000

    Codificacion delta funcionando

--- trunk/TP/src/codificacion.cpp
+++ trunk/TP/src/codificacion.cpp
@@ -1,29 +1,29 @@
-#include <cstddef>
 #include "codificacion.h"
+#include <cmath>
 
 
 CDelta::CDelta(unsigned long int numero) {
-	uchar* delta[50];
+	uchar delta[50];
 	bits = base_code(numero,gamma_code,delta);
 	bytes = (bits/8);
 	if(bits%8 != 0)
 		bytes += 1;
 	codigo = new uchar[bytes];
-	escribirBinario(codigo, bytes, delta, bits);	
+	escribirBinario(codigo, delta, bits);	
 }
 
 CDelta::~CDelta() {
 	delete []codigo;
 }
 
-void CDelta::escribirBinario(uchar* binario, size_t bytes uchar* code, size_t bits) {
+void CDelta::escribirBinario(uchar* binario, uchar* code, size_t bits) {
 	uchar buffer = 0;
 	int bit_actual = 0;
 	int posArray = 0;
 	int nByte = 0;
 	while(posArray < (int)bits) {
-		if((x+1)%8 == 0) {
-			if(array[posArray] & 1)
+		if((posArray+1)%8 == 0) {
+			if(code[posArray] & 1)
 				buffer |= (1<<bit_actual);
 			binario[nByte] = buffer;
 			nByte++;
@@ -32,23 +32,25 @@ void CDelta::escribirBinario(uchar* binario, size_t bytes uchar* code, size_t bi
 			posArray++;
 			continue;
 		}
-		if(array[posArray] & 1)
+		if(code[posArray] & 1)
 			buffer |= (1<<bit_actual);
 		posArray++;
 		bit_actual++;
 	}
+	int flag = 0;
 	while(posArray%8 != 0) {
 		buffer |= (1<<bit_actual);
 		posArray++;
 		bit_actual++;
+		flag = 1;
 	}
-	if(posArray%8 == 0) {
+	if(posArray%8 == 0 && flag == 1) {
 		binario[nByte] = buffer;
 	}
 	return;
 }
 
-int CDelta::unary_code(long int number, uchar* unary) {
+int unary_code(long int number, uchar* unary) {
 	int x = number - 1;
 	int p = 0;
 	while(p < x) {
@@ -60,7 +62,7 @@ int CDelta::unary_code(long int number, uchar* unary) {
 	return (int)number;
 }
 
-int CDelta::binary_code(long int number, int bits, uchar* binary) {
+int binary_code(long int number, int bits, uchar* binary) {
 	int division = number;
 	
 	for(int x = 0; x < bits; x++) {
@@ -76,7 +78,7 @@ int CDelta::binary_code(long int number, int bits, uchar* binary) {
 	return bits;
 }
 
-int CDelta::base_code(long int number, int func(long int,uchar*), uchar* vec) {
+int base_code(long int number, int func(long int,uchar*), uchar* vec) {
 	int bits = (int)floor(log10(number)/log10(2)); // log(number) en base 2
 	int q = 1+bits;
 	func(q,vec);
@@ -98,6 +100,6 @@ int CDelta::base_code(long int number, int func(long int,uchar*), uchar* vec) {
 	return (pre+bits);
 }
 
-int CDelta::gamma_code(long int number, uchar* gamma) {
+int gamma_code(long int number, uchar* gamma) {
 	return base_code(number,unary_code,gamma);
 }
--- trunk/TP/src/codificacion.h
+++ trunk/TP/src/codificacion.h
@@ -1,6 +1,8 @@
 #ifndef CODIFICACION_H
 #define CODIFICACION_H
 
+#include <cstddef>
+
 typedef unsigned char uchar;
 
 class CDelta {
@@ -9,26 +11,8 @@ class CDelta {
 		int bits;
 		int bytes;
 		
-		// Escribe un numero en unario
-		// recibe un buffer en donde depositara el codigo unario
-		int unary_code(long int number, uchar* unary);
-		
-		// Escribe un numero en binario con la cantidad de bits pedida
-		// Recibe un buffer en donde depositara el codigo binario
-		// Pre: el numero debe poder representarse con la cantidad de bits indicada
-		// Post: El codigo binario devuelto se lee de izquierda a derecha
-		int binary_code(long int number, int bits, uchar* binary);
-		
-		// Funcion que genera un codigo con una primera parte en codigo especificado (func)
-		// y la segunda parte en codigo binario
-		int base_code(long int number, int func(long int,uchar*), uchar* vec);
-		
-		// Escribe un numero en codigo gamma
-		// Recibe un buffer en el cual se depositara el codigo gamma
-		int gamma_code(long int number, uchar* gamma);
-		
 		// Escribe un codigo a una tira de bytes
-		void escribirBinario(uchar* bytes, uchar* code, size_t size);
+		void escribirBinario(uchar* binario, uchar* code, size_t bits);
 		
 	public:
 		
@@ -36,6 +20,24 @@ class CDelta {
 		CDelta(unsigned long int numero);
 		
 		virtual ~CDelta();
+};
 
+// Escribe un numero en unario
+// recibe un buffer en donde depositara el codigo unario
+int unary_code(long int number, uchar* unary);
+
+// Escribe un numero en binario con la cantidad de bits pedida
+// Recibe un buffer en donde depositara el codigo binario
+// Pre: el numero debe poder representarse con la cantidad de bits indicada
+// Post: El codigo binario devuelto se lee de izquierda a derecha
+int binary_code(long int number, int bits, uchar* binary);
+
+// Funcion que genera un codigo con una primera parte en codigo especificado (func)
+// y la segunda parte en codigo binario
+int base_code(long int number, int func(long int,uchar*), uchar* vec);
+
+// Escribe un numero en codigo gamma
+// Recibe un buffer en el cual se depositara el codigo gamma
+int gamma_code(long int number, uchar* gamma);
 
 #endif
