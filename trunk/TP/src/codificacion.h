9ff69f430755a907f6e68f790949392ad3638b6c
fedeagb@gmail.com <fedeagb@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 09:24:11 +0000

    Funciones de decodificado traidas de C

--- trunk/TP/src/codificacion.h
+++ trunk/TP/src/codificacion.h
@@ -7,20 +7,24 @@ typedef unsigned char uchar;
 
 class CDelta {
 	private:
-		uchar *codigo;
-		int bits;
-		int bytes;
-		
 		// Escribe un codigo a una tira de bytes
 		void escribirBinario(uchar* binario, uchar* code, size_t bits);
 		
 	public:
+		uchar *codigo;
+		int bits;
+		int bytes;
 		
 		// Constructor de la clase
 		CDelta(unsigned long int numero);
 		
+		// Destructor de la clase
 		virtual ~CDelta();
 };
+
+/****************************
+* Funciones codificacion
+*****************************/
 
 // Escribe un numero en unario
 // recibe un buffer en donde depositara el codigo unario
@@ -39,5 +43,22 @@ int base_code(long int number, int func(long int,uchar*), uchar* vec);
 // Escribe un numero en codigo gamma
 // Recibe un buffer en el cual se depositara el codigo gamma
 int gamma_code(long int number, uchar* gamma);
+
+/****************************
+* Funciones decodificacion
+*****************************/
+
+// Decodifica un codigo unario
+long int unary_decode(uchar* unary);
+
+// Decodifica un codigo binario
+long int binary_decode(uchar* binary);
+
+// Decodifica un codigo gamma, y se le pasa un puntero a un entero para saber
+// cuantos uchar decodifico del array
+long int gamma_decode(uchar* gamma, int* done);
+
+// Decodifica un codigo delta
+long int delta_decode(uchar* delta);
 
 #endif
