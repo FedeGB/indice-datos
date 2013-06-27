bc5b7def68064f7b7b9384ae6f92138722173a66
ivanjqk@gmail.com <ivanjqk@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 00:27:58 +0000

    Iván creando el proyecto con Eclipse.

---
+++ trunk/TP/src/TP.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TP.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+// https://docs.google.com/file/d/0B8rBD4QSqWnSdjlEa2V3YXV0WHc/edit?usp=sharing
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
