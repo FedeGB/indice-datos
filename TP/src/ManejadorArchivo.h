309982034eff1fdbe73ce3030cf4cb43284a04f2
ivanjqk@gmail.com <ivanjqk@gmail.com@9649ce06-104a-2bdd-eb08-35d52d920642>
2013-06-27 02:45:13 +0000

    Iván agregando algunas clases básicas.

--- TP/src/ManejadorArchivo.h
+++ TP/src/ManejadorArchivo.h
@@ -1,3 +1,6 @@
+#ifndef MANEJADORARCHIVO_H_
+#define MANEJADORARCHIVO_H_
+
 /*
  * ManejadorArchivo.h
  *
@@ -5,22 +8,19 @@
  *      Author: GRUPO 19
  */
 
-#ifndef MANEJADORARCHIVO_H_
-#define MANEJADORARCHIVO_H_
-
 #include <fstream>
 #include <string>
 
 class ManejadorArchivo {
-public:
-	std::fstream archivoHandler;
-	char buffer[1];
-	char bitsEnBuffer;
+  public:
+    std::fstream archivoHandler;
+    char buffer[1];
+    char bitsEnBuffer;
 
-	ManejadorArchivo(const std::string &archivoRuta);
-	virtual ~ManejadorArchivo();
+    ManejadorArchivo(const std::string &archivoRuta);
+    virtual ~ManejadorArchivo();
 
-	void escribir(const char * const datos, const int cantidadBits);
+    void escribir(const char * const datos, const int cantidadBits);
 };
 
 #endif /* MANEJADORARCHIVO_H_ */
---
+++ TP/src/RetornoCodigos.h
@@ -0,0 +1,17 @@
+#ifndef VALORESRETORNO_H_
+#define VALORESRETORNO_H_
+
+/*
+ * RetornoCodigos.h
+ *
+ *  Created on: 27 Jun 2013
+ *      Author: GRUPO 19
+ */
+
+#define RESULTADO_OK 2
+#define ERROR -2
+#define ERROR_EXCEPCION -3
+#define ERROR_ARGC -4
+#define ERROR_ARGV -5
+
+#endif /* VALORESRETORNO_H_ */
